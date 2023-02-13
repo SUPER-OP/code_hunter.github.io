5fd438b4bd475e0e9aac3a0cf5c2a732d9fd5999
SUPER-OP <107782754+SUPER-OP@users.noreply.github.com>
2023-02-13 08:51:38 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   trailingSlash:true
+// }
+// module.exports = nextConfig
+const repo = 'change-me-to-your-repo'
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
+module.exports={
+  reactStrictMode:true,
+  trailingSlash:true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
