8bef9564fb229529133427d248d808007c135200
SUPER-OP <107782754+SUPER-OP@users.noreply.github.com>
2023-02-13 18:08:53 +0530

    Update getblog.js

--- src/pages/api/getblog.js
+++ src/pages/api/getblog.js
@@ -2,7 +2,7 @@
 import * as fs from 'fs'
 
 export default function handler(req, res) {
-  fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data) => {
+  fs.readFile(`blogpost/${req.query.slug}.json`, 'utf-8', (err, data) => {
     if(err){
       
       res.status(500).json({error:"BLOG NOT FOUND"})
